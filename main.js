ce33ba09d1fcae4c956cc65aa947130fc2452079
Mark Valdez <Mark Valdez>
2016-07-29 12:32:31 -0700

    fix main.js and make it functional, additional fixes in comments

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
 $(document).ready(function(){
-   $('select').on('change', function () {
-   event.preventDefault(),
-   var selection = document.getElementsByTagName('option').value;
-   console.log(selection);
+  console.log($);
 
-   var url = 'https://api.nytimes.com/svc/topstories/v2/' + $('.dropdown-menu').value() + '.json';
-   url += '?' + $.param({'api-key': 'f998b2b22bf342ba8fd3f6c6da7560a8'});
+  $('select').on('change', function () {
+  event.preventDefault();
+  var selection = document.getElementById('dropdownOption').value;
+  console.log(selection);
+
+  var url = 'https://api.nytimes.com/svc/topstories/v2/' + selection + '.json'; // you already have var 'selection', so just input it here
+      url += '?' + $.param({'api-key': 'f998b2b22bf342ba8fd3f6c6da7560a8'});
+
     $.ajax({
-     url: url,
-     method: 'GET',
-     dataType: 'json',
-     })
-     .done(function (data) {
-            if (data.results.length === 0) {
-                $('.results').append('<p>Sorry,  please try again.</p>');
-            } else {
-                var nytData = data.results.filter(function (item) {
-                  if (nytData.length !== 0) {
-                    nytItems += '<ul>';
+      method: 'GET',
+      url: url
+    })
 
-                    $.each(nytData, function(key, value) {
+    .done(function(data) {
+      console.log(data); // check the data
 
-                      if (value.multimedia.length)  {
+      if (data.results.length === 0) {
+          $('.results').append('<p>Sorry,  please try again.</p>');
+      } 
+      
+      // else {
+      //     var nytData = data.results.filter(function (item) {
+      //         if (nytData.length !== 0) {
+      //             nytItems += '<ul>';
 
-                        articleImageUrl = value.multimedia[4].url;
-                        articleCaption = value.abstract;
-                        articleLink = value.url;
+      //             $.each(nytData, function(key, value) {
 
-                        nytItems += '<li>'
-                        nytItems += '<a href=="' + articleLink + '" target="_blank">'
-                        nytItems += '<div class="inner-tem-wrapper">';
-                        nytItems += '<div class="article" style="background-image:url('+articleImageUrl+')">';
-                        nytItems += '<div class="photo-meta">';
-                        nytItems += '</div>'
-                        nytItems += '</div>'
+      //                 if (value.multimedia.length)  {
+      //                   articleImageUrl = value.multimedia[4].url;
+      //                   articleCaption = value.abstract;
+      //                   articleLink = value.url;
 
-                 };
-                 });
-                 };
-                 });
-                 });
-
-
+      //                   nytItems += '<li>'
+      //                   nytItems += '<a href=="' + articleLink + '" target="_blank">'
+      //                   nytItems += '<div class="inner-tem-wrapper">';
+      //                   nytItems += '<div class="article" style="background-image:url('+articleImageUrl+')">';
+      //                   nytItems += '<div class="photo-meta">';
+      //                   nytItems += '</div>'
+      //                   nytItems += '</div>'
+      //                 };
+      //             });
+      //         };
     });
+
+  });
+
+
+});
